87337620c5539d2ed0f55a73520fb17d7b209b33
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-03 01:13:54 +0530

    Added basic Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import MainRoutes from './routes/MainRoutes';
+import Navbar from './components/Navbar';
 
 function App() {
 
   console.log(import.meta.env.VITE_API_KEY); 
   return (
     <>
-      <nav>Simple nav</nav>
+      <Navbar/>
       <MainRoutes />
     </>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+function Home() {
+
+  return (
+      <>
+          {/* navbar */}
+          {/* Movielist */}
+          {/* pagination buttons */}
+          Home
+      </>
+  )
+}
+
+export default Home;
