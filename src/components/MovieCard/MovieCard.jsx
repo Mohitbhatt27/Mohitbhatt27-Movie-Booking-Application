1915eb602c3e7f9b09ab6668ff727a706d40c48b
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-03 17:27:25 +0530

    Added Movie card UPI

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./MovieCard.css";
+
+function MovieCard({ Title, Year, imdbID, Type, Poster }) {
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-image">
+        <img src={Poster} />
+      </div>
+      <div className="movie-title">
+        <span>{Title}</span>
+      </div>
+      <div className="movie-year">
+        <span>Released in: {Year}</span>
+      </div>
+      <div className="movie-type">
+        <span>Type: {Type}</span>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,24 @@
+import MovieCard from "../components/MovieCard/MovieCard";
 import "./Home.css";
 
 function Home() {
-
+  const movie = {
+    Title: "Shaitaan",
+    Year: "2024",
+    imdbID: "tt27744786",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYjczYjQ3ZTMtMDAwZi00Y2I2LWE2MTMtOGUyM2YyMThmYTFiXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+  };
   return (
-      <>
-          {/* navbar */}
-          {/* Movielist */}
-          {/* pagination buttons */}
-          Home
-      </>
-  )
+    <>
+      <div className="movie-card-wrapper">
+        <MovieCard {...movie} />
+        <MovieCard {...movie} />
+        <MovieCard {...movie} />
+      </div>
+    </>
+  );
 }
 
 export default Home;
